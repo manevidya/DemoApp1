bedd233df30ca589bed6ec026020fee1376c8d0d
vidya <vidya@gmail.com>
2025-01-04 17:26:20 +0530

    Add new File

--- DemoApp1/Models/Demo.cs
+++ DemoApp1/Models/Demo.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }    
+        public string NewTital { get; set; }
+        public string NewURL { get; set; }
         public string Description { get; set; }
     }
 }
