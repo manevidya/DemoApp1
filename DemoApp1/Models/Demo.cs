19bf2a29d2aab0a2323e72910344b8d51ed5f489
manevidya <161304648+manevidya@users.noreply.github.com>
2025-01-04 17:40:33 +0530

    Add new File (#5)
    Co-authored-by: vidya <vidya@gmail.com>

--- DemoApp1/Models/Demo.cs
+++ DemoApp1/Models/Demo.cs
@@ -4,7 +4,12 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }    
+
+        public string NewTital { get; set; }
+        public string NewURL { get; set; }
+
         public string Title { get; set; }
+
         public string Description { get; set; }
     }
 }
