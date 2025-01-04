d9df8fb7c5cb115780cd0f9a0c326d716151f347
manevidya <161304648+manevidya@users.noreply.github.com>
2025-01-04 17:36:44 +0530

    Revert "Add new File (#3)"
    This reverts commit 8e283ee0bdf3353df968276a21b3156ca2049c6f.

--- DemoApp1/Models/Demo.cs
+++ DemoApp1/Models/Demo.cs
@@ -4,12 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }    
-
-        public string NewTital { get; set; }
-        public string NewURL { get; set; }
-
         public string Title { get; set; }
-
         public string Description { get; set; }
     }
 }
