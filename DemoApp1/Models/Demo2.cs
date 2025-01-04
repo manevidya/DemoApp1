413509cbe16252c1f7dff3cd6aee460b76515c3e
vidya <vidya@gmail.com>
2025-01-04 17:47:52 +0530

    Add new File Demo2

---
+++ DemoApp1/Models/Demo2.cs
@@ -0,0 +1,11 @@
+﻿namespace DemoApp1.Models
+{
+    public class Demo2
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string AuthorId { get; set; }
+    }
+}
