d26ea791adb76cfdbf5214d03efb08be4348e5f2
manevidya <161304648+manevidya@users.noreply.github.com>
2025-01-05 19:25:35 +0530

    Demo7 (#13)
    * add demo5 file
    
    * add new file demo5
    
    * add a new file
    
    ---------
    
    Co-authored-by: vidya <vidya@gmail.com>

---
+++ DemoApp1/Models/demo6.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoApp1.Models
+{
+    public class demo6
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+
+    }
+}
